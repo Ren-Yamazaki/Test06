8256a75bce9d0fa81d497b90067d92b233815417
al19059 <84760939+al19059@users.noreply.github.com>
2021-07-12 21:20:30 +0900

    Update HabitMain_C.py

--- app/HabitMain_C.py
+++ app/HabitMain_C.py
@@ -1,8 +1,8 @@
 '''
 *  File Name		: HabitMain.py
-*  Version		: V1.0
-*  Designer		: 榎並龍大
-*  Date			: 2021.06.02
+*  Version		: V1.1
+*  Designer		: 榎並 龍大
+*  Date			: 2021.07.12
 *  Purpose       	: 新規登録画面から読み取ったデータをサーバへ出す。
 '''
 from flask import Flask, render_template, request,  redirect ,url_for,Blueprint,flash
@@ -14,10 +14,12 @@ from app import db
 
 hmain=Blueprint('habitmain',__name__)
 
+#習慣化登録
 @hmain.route('/create')
 def create():
     return render_template('create_h.html')
 
+#データ削除
 @hmain.route('/delete/<int:id>')
 def delete(id):
     post = Post.query.get(id)
