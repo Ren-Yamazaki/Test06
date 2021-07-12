3690e3ab77fd381da3367991ecaf41dec472ae87
al19059 <84760939+al19059@users.noreply.github.com>
2021-07-12 21:04:55 +0900

    Update HabitMain_C.py

--- app/HabitMain_C.py
+++ app/HabitMain_C.py
@@ -1,8 +1,8 @@
 '''
 *  File Name		: HabitMain.py
 *  Version		: V1.0
-*  Designer		: 榎並龍大
-*  Date			: 2021.06.02
+*  Designer		: 榎並　龍大
+*  Date			: 2021.07.12
 *  Purpose       	: 新規登録画面から読み取ったデータをサーバへ出す。
 '''
 from flask import Flask, render_template, request,  redirect ,url_for,Blueprint
